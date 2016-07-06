24e75b5487579f207fc798c20908de0619d2ad03
my88899 <haojiutao@gmail.com>
2016-07-06 21:24:51 +0800

    Ad03 finished

--- Ad03/double.py
+++ Ad03/double.py
@@ -2,7 +2,7 @@ import Tkinter as tk
 window =tk.Tk()
 def changeString():
 	stringToCopy =entry.get()
-	entry.insert(1, stringToCopy)
+	entry.insert(0, stringToCopy)
 
 entry = tk.Entry(window)
 button =tk.Button(window, text="Change", command=changeString)
---
+++ Ad03/guessNum.py
@@ -0,0 +1,39 @@
+import Tkinter as tk
+import random
+window=tk.Tk()
+
+maxNo  =10
+score  =0
+rounds =0
+
+def buttonClick():
+	global score
+	global rounds
+	try:
+		guess =int(guessBox.get())
+		if 0<guess<=maxNo:
+			result=random.randrange(1,maxNo+1)
+			if guess == result:
+				score =score+1
+			rounds = rounds +1
+		else:
+			result ="Entry not valid"
+	except :
+		result ="Entry not valid"
+	resultLabel.config(text=result)
+	scoreLabel.config(text=score)
+	guessBox.delete(0, tk.END)
+
+guessLabel =tk.Label(window, text="Enter a number from 1 to " + str(maxNo))
+guessBox =tk.Entry(window)
+resultLabel=tk.Label(window)
+scoreLabel=tk.Label(window)
+button=tk.Button(window, text="guess", command=buttonClick)
+
+guessLabel.pack()
+guessBox.pack()
+resultLabel.pack()
+scoreLabel.pack()
+button.pack()
+
+window.mainloop()
