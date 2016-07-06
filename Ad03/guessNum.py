6a70e21d5aef7e2cdc8783b74d19afdfc2ed41d5
my88899 <haojiutao@gmail.com>
2016-07-06 21:43:19 +0800

    for dormitory test Ad04

--- Ad03/guessNum.py
+++ Ad03/guessNum.py
@@ -20,15 +20,15 @@ def buttonClick():
 			result ="Entry not valid"
 	except :
 		result ="Entry not valid"
-	resultLabel.config(text=result)
-	scoreLabel.config(text=score)
+	resultLabel.config(text =result)
+	scoreLabel.config(text  =score)
 	guessBox.delete(0, tk.END)
 
-guessLabel =tk.Label(window, text="Enter a number from 1 to " + str(maxNo))
-guessBox =tk.Entry(window)
-resultLabel=tk.Label(window)
-scoreLabel=tk.Label(window)
-button=tk.Button(window, text="guess", command=buttonClick)
+guessLabel  =tk.Label(window, text="Enter a number from 1 to " + str(maxNo))
+guessBox    =tk.Entry(window)
+resultLabel =tk.Label(window)
+scoreLabel  =tk.Label(window)
+button      =tk.Button(window, text="guess", command=buttonClick)
 
 guessLabel.pack()
 guessBox.pack()
---
+++ Ad04/slider.py
@@ -0,0 +1,20 @@
+import Tkinter as tk
+window=tk.Tk()
+
+redDirection ="up"
+greenDirection ="up"
+blueDirextion ="up"
+
+red =0
+green=0
+blue=0
+
+def colourUpdate():
+	global red
+	global green
+	global blue
+
+	global redDirection
+	global greenDirection
+	global blueDirextion
+	pass
